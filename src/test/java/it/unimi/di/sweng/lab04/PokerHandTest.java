a50c69c82d30deb1f997d89171aad6b3d8a6b973
Luca Dal Mas <luca.dalmas@studenti.unimi.it>
2024-11-08 14:45:20 +0100

    ROSSO: PokerHand

--- src/test/java/it/unimi/di/sweng/lab04/PokerHandTest.java
+++ src/test/java/it/unimi/di/sweng/lab04/PokerHandTest.java
@@ -11,14 +11,26 @@ import ca.mcgill.cs.stg.solitaire.cards.Suit;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
+import java.util.List;
+
 
 @Timeout(2)
 public class PokerHandTest {
 
+  private static Deck createEmptyDeck() {
+    Deck deck = new Deck();
+    for (int i = 0; i<52;i++)deck.draw();
+    return deck;
+  }
+  final static private Card[] THREE_OF_A_KIND_CARDS = {
+          Card.get (Rank.ACE, Suit. HEARTS), Card.get (Rank. THREE, Suit. HEARTS) , Card. get (Rank. FIVE, Suit. HEARTS), Card.get (Rank. ACE, Suit. CLUBS),
+          Card.get (Rank. ACE, Suit. DIAMONDS)
+  };
 
   @Test
   void newPokerHandTest() {
-
+    PokerHand hand = new PokerHand(List.of(THREE_OF_A_KIND_CARDS));
+    assertThat(hand).contains(null);
   }
 
 }
